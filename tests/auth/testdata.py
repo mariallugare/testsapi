d637e9747da729f4a0582e1a1da894c07c2df623
MarialLugare <mariallugare@gmail.com>
2024-01-22 05:49:14 +0300

    first commit

---
+++ tests/auth/testdata.py
@@ -0,0 +1,13 @@
+# Authentication data
+AUTH_DATA = {
+    "username": "admin",
+    "password": "password123"
+}
+# Wrong auth data
+WRONG_AUTH_DATA = {
+    "username": "111",
+    "password": "111"
+}
+
+# Base URL
+BASE_URL = "https://restful-booker.herokuapp.com"
